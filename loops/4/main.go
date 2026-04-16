2e2a6a7092ee13aa71e727ccaed57d641a32c681
Tanush-lol <tanushroy1@gmail.com>
2026-04-17 01:44:14 +0530

    day5

---
+++ loops/4/main.go
@@ -0,0 +1,28 @@
+
+package main
+
+import "fmt"
+
+func fizzbuzz() {
+	for i := 1; i <= 100; i++ {
+		output := ""
+
+		if i%3 == 0 {
+			output += "fizz"
+		}
+		if i%5 == 0 {
+			output += "buzz"
+		}
+
+		if output == "" {
+			fmt.Println(i)
+		} else {
+			fmt.Println(output)
+		}
+}}
+
+// don't touch below this line
+
+func main() {
+	fizzbuzz()
+}
